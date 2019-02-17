9d58cfa37f8c8fd726609cba9117e0628ddd9dba
Salah Eddine <seddin@users.noreply.github.com>
2019-02-17 16:10:01 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# pruebas2
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1 className="title">Future Lab</h1>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
